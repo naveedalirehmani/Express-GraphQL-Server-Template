86edb831f0708f1ad5b57ab91032e1370f3da2f1
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-01-25 22:48:45 +0500

    added query authorization check in graphql context

--- graphql/context.ts
+++ graphql/context.ts
@@ -4,7 +4,12 @@ import { AuthenticationError, gql } from 'apollo-server-express';
 async function context(request: Request, response: Response) {
   const result: any = gql(request.body.query);
 
-
+  if (result) {
+    let queryName = result.definitions[0].selectionSet.selections[0].name.value;
+    console.log("QueryName", queryName);
+  } else {
+    throw new AuthenticationError("Unauthorized");
+  }
 
   return {
     request,
