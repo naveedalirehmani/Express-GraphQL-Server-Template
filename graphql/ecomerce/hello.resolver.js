908ce470ca301bfbcca31f913dcf05700470033b
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-01-25 22:47:47 +0500

    added resolver.js

--- graphql/ecomerce/hello.resolver.js
+++ graphql/ecomerce/hello.resolver.js
@@ -2,9 +2,16 @@ const resolver = {
   Query:{
     hello(parent,args,context){
       console.log('called')
-      return 'hello graphql'
+      return 'hello graphql server'
     }
-  }
+  },
+  // Subscription:{
+  //   hello:{
+  //     async subscribe(parent,context,{pubSub}){
+  //       return pubSub.asyncIterator('HELLO_CALLED')
+  //     }
+  //   }
+  // }
 }
 
 
