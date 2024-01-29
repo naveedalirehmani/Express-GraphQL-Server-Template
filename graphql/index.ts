34a1bd878b6c6d3c398ac271b3fc0d6458c7c49e
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-01-29 20:15:09 +0500

    stiching resolvers and schema

--- graphql/index.ts
+++ graphql/index.ts
@@ -7,3 +7,9 @@ import { GraphQLSchema } from "graphql";
 const typeDefs = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const resolvers = loadFilesSync(path.join(__dirname, "**/*.resolver.js"));
 
+const executableSchema: GraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+export { executableSchema };
